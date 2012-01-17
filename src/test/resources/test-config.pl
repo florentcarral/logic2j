3ac5d0f16e4954589e095debbcb3ac867e6e6256
Victor Pirat <pirat.victor@gmail.com>
2012-01-17 09:50:57 +0100

    Make the absolute path relative.

--- src/test/resources/test-config.pl
+++ src/test/resources/test-config.pl
@@ -1,1 +1,1 @@
-initialize :- rdb_config("org.apache.derby.jdbc.AutoloadedDriver", "jdbc:derby:D:/Documents/dev/java/logic2j-fork/src/test/db/zipcodes1/derby-v10.8.2.1", "APP", "APP", "zipcodesdb_", ["ZIP_CODE"]).
+initialize :- rdb_config("org.apache.derby.jdbc.AutoloadedDriver", "jdbc:derby:src/test/db/zipcodes1/derby-v10.8.2.1", "APP", "APP", "zipcodesdb_", ["ZIP_CODE"]).
