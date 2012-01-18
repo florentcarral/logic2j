5e814a0832d192edb426694a264d8135f0e6d787
Etienne Mendes <mendes.etienne@gmail.com>
2012-01-18 13:42:58 +0100

    Adding specific tests for the translation predicate created (TranslationLibrary

--- src/test/java/org/logic2j/library/impl/webservice/TranslationLibraryTest.java
+++ src/test/java/org/logic2j/library/impl/webservice/TranslationLibraryTest.java
@@ -4,20 +4,21 @@ import org.junit.Test;
 import org.logic2j.PrologTestBase;
 import org.logic2j.library.impl.io.IOLibrary;
 
-public class TranslationLibraryTest extends PrologTestBase{
+public class TranslationLibraryTest extends PrologTestBase {
 
-    
     @Test
     public void translation() throws Exception {
         loadLibrary(new IOLibrary(getProlog()));
         loadLibrary(new TranslationLibrary(getProlog()));
         System.out.println("\n1st test : ");
-        assertNSolutions(1,"X='Ciao',translate('Hello world!', 'en', 'it', X)");
+        assertNSolutions(1, "X='Ciao',translate('Hello world!', 'en', 'it', X)");
         System.out.println("\n2nd test : ");
         assertNoSolution("X='Ciaos',translate('Hello world!', 'en', 'it', X)");
         System.out.println("\n3nd test : ");
-        assertNSolutions(1,"X='Salut Bob',translate('Hi%20Bob!', 'en', 'fr', X), write(X)");
+        assertNSolutions(1, "X='Salut Bob',translate('Hi%20Bob!', 'en', 'fr', X), write(X)");
         System.out.println("\n4th test : ");
-        assertNSolutions(1,"X='Je peux vous appeler quand vous voulez.',translate('I%20can%20call%20you%20whenever%20you%20want.', 'es', 'fr', X), write(X)");
+        assertNSolutions(
+                1,
+                "X='Je peux vous appeler quand vous voulez.',translate('I%20can%20call%20you%20whenever%20you%20want.', 'es', 'fr', X), write(X)");
     }
 }
