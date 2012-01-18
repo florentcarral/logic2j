ecdf57e1f1d6404407ca02f58a4978d346c7ded9
Victor Pirat <pirat.victor@gmail.com>
2012-01-18 12:31:42 +0100

    Add a new line after writing to the console.

--- src/test/java/org/logic2j/library/impl/webservice/DetectLanguageLibraryTest.java
+++ src/test/java/org/logic2j/library/impl/webservice/DetectLanguageLibraryTest.java
@@ -10,8 +10,8 @@ public class DetectLanguageLibraryTest extends PrologTestBase {
     public void detectionTest() throws Exception {
         loadLibrary(new IOLibrary(getProlog()));
         loadLibrary(new DetectLanguageLibrary(getProlog()));
-        assertNSolutions(1, "detect_language('Hello world !', Language), write(Language)");
-        assertNSolutions(1, "Text = 'Salut, ma maison est bleue !', detect_language(Text, Language), write(Language)");
+        assertNSolutions(1, "detect_language('Hello world !', Language), write(Language), nl");
+        assertNSolutions(1, "Text = 'Salut, ma maison est bleue !', detect_language(Text, Language), write(Language), nl");
     }
     
     @Test
@@ -19,7 +19,7 @@ public class DetectLanguageLibraryTest extends PrologTestBase {
         loadLibrary(new IOLibrary(getProlog()));
         loadLibrary(new DetectLanguageLibrary(getProlog()));
         loadLibrary(new TranslationLibrary(getProlog()));
-        assertNSolutions(1, "Text = 'Salut,%20ma%20maison%20est%20bleue%20!', detect_language(Text, Language), translate(Text, Language, 'en', TranslatedText), write(TranslatedText)");
+        assertNSolutions(1, "Text = 'Salut,%20ma%20maison%20est%20bleue%20!', detect_language(Text, Language), translate(Text, Language, 'en', TranslatedText), write(TranslatedText), nl");
     }
     
 }
