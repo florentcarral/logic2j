9cb644c2cb534c80776b45b4f27760de139a3e50
Etienne Mendes <mendes.etienne@gmail.com>
2012-01-18 15:12:18 +0100

    deleting the old tests (when two primitives were used for the YahooMapLibrary service

--- src/test/java/org/logic2j/library/impl/webservice/YahooMapLibraryTest.java
+++ src/test/java/org/logic2j/library/impl/webservice/YahooMapLibraryTest.java
@@ -28,23 +28,6 @@ public class YahooMapLibraryTest extends PrologTestBase {
         System.out.println("\n 8th test : ");
         assertNoSolution("yahoomap(ADDRESS, LONGITUDE,'244 chemin de la Cassine,73000 Chambéry', 'MY_API_KEY'), write(LATITUDE), write(LONGITUDE)");
     }
-    
-    
-    @Test
-    public void yahoomap_address() throws Exception {
-        loadLibrary(new IOLibrary(getProlog()));
-        loadLibrary(new YahooMapLibrary(getProlog()));
-        System.out.println("\n 1st test : ");
-        assertNSolutions(1,"yahoomap_address(X, '12.05435', '13.2345', 'MY_API_KEY'), write(X)");
-        System.out.println("\n 2nd test : ");
-        assertNoSolution("yahoomap_address('a fake address', '12.05435', '13.2345', 'MY_API_KEY'), write(X)");
-        System.out.println("\n 3rd test : ");
-        assertNSolutions(1,"yahoomap_address(X, Y, '13.2345', 'MY_API_KEY'), write(X), write('    and Y =>'), write(Y)");
-        System.out.println("\n 4th test : ");
-        assertNSolutions(1,"Y='12.05435', yahoomap_address(X, Y, '13.2345', 'MY_API_KEY'), write(X), write('    and Y =>'), write(Y)");
-        System.out.println("\n 5th test : ");
-        assertNoSolution("yahoomap_address(X,'0', '0', 'MY_API_KEY')");
-    }
 
     @Test
     public void yahoomap_coordinates() throws Exception {
